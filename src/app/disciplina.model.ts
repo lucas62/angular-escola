c9e27cc7020f7bd8ad64880c580a3598c5013cbc
lucas <lucas.pires100.la@gmail.com>
2018-03-04 01:55:41 -0300

    livro-interacao-codigo

--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -1,9 +1,14 @@
+import {Professor} from './professor.model';
 export class Disciplina {
+  cod: number;
   nome: string;
+  professor: Professor;
   descricao: string;
 
-  constructor(nome: string, descricao?: string) {
+  constructor(cod: number,nome: string,professor: Professor,descricao?: string) {
+    this.cod = cod;
     this.nome = nome;
+    this.professor = professor;
     this.descricao = descricao;
   }
 }
