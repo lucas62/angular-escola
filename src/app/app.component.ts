46c0ed4d53966953df78d58d94d780810dbde0ce
lucas62 <lucas.pires100.la@gmail.com>
2018-03-17 20:22:00 -0300

    CRUD+LS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Disciplina} from './disciplina.model';
+import { discardPeriodicTasks } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +8,27 @@ import {Disciplina} from './disciplina.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  visualizado = false;
   editando = null;
+  codigo = null;
   nome = null;
   descricao = null;
+  data = null;
+  ativa = null;
+  tipo = 'principal';
+  periodo = null;
   excluir_ok = false;
   editar_ok = false;
   salvar_ok = false;
   disciplinas = [
-    new Disciplina('Língua Portuguesa', 'O objetivo norteador da BNCC de ' +
+    new Disciplina(1,'Língua Portuguesa', 'O objetivo norteador da BNCC de ' +
       'Língua Portuguesa é garantir a todos os alunos o acesso aos saberes ' +
       'linguísticos necessários para a participação social e o exercício da ' +
       'cidadania, pois é por meio da língua que o ser ' +
       'humano pensa, comunica-se, tem acesso à informação, expressa e ' +
       'defende pontos de vista, partilha ou constrói visões de mundo e ' +
       'produz conhecimento.'),
-    new Disciplina('Educação Física', 'A Educação Física é o componente ' +
+    new Disciplina(2,'Educação Física', 'A Educação Física é o componente ' +
       'curricular que tematiza as práticas corporais em suas diversas formas ' +
       'de codificação e significação social, entendidas como manifestações ' +
       'das possibilidades expressivas dos sujeitos e patrimônio cultural ' +
@@ -30,7 +37,7 @@ export class AppComponent {
       'um deslocamento espaço-temporal de um segmento corporal ' +
       'ou de um corpo todo. Logo, as práticas corporais são textos culturais ' +
       'passíveis de leitura e produção.'),
-    new Disciplina('Inglês', 'Aprender a língua inglesa propicia a criação ' +
+    new Disciplina(3, 'Inglês', 'Aprender a língua inglesa propicia a criação ' +
       'de novas formas de engajamento e participação dos alunos em um mundo ' +
       'social cada vez mais globalizado e plural, em que as fronteiras ' +
       'entre países e interesses pessoais, locais, regionais, nacionais ' +
@@ -42,7 +49,7 @@ export class AppComponent {
       'formativo que inscreve a aprendizagem de inglês em uma perspectiva ' +
       'de educação linguística, consciente e crítica, na qual as dimensões ' +
       'pedagógicas e políticas são intrinsecamente ligadas.'),
-    new Disciplina('Matemática', 'No Ensino Fundamental, essa área, por ' +
+    new Disciplina(4, 'Matemática', 'No Ensino Fundamental, essa área, por ' +
       'meio da articulação de seus diversos campos – Aritmética, Álgebra, ' +
       'Geometria, Estatística e Probabilidade – precisa garantir que os ' +
       'alunos relacionem observações empíricas do mundo real a ' +
@@ -56,7 +63,7 @@ export class AppComponent {
       'algumas propriedades e a verificação de conjecturas, a partir ' +
       'de outras, podem ser estimuladas, sobretudo ao final do ' +
       'Ensino Fundamental.'),
-    new Disciplina('Ciências', 'Ao estudar Ciências, as pessoas aprendem ' +
+    new Disciplina(5, 'Ciências', 'Ao estudar Ciências, as pessoas aprendem ' +
       'a respeito de si mesmas, da diversidade e dos processos de evolução ' +
       'e manutenção da vida, do mundo material – com os seus recursos ' +
       'naturais, suas transformações e fontes de energia –, do nosso ' +
@@ -66,23 +73,39 @@ export class AppComponent {
       'compreendam, expliquem e intervenham no mundo em que vivem.')
   ];
 
+  visualizar() {
+    this.redefinir();
+    this.visualizado = true;
+  }
+
   salvar() {
+    const situacao = this.ativa;
     if (this.editando) {
+      this.editando.codigo = this.codigo;
       this.editando.nome = this.nome;
       this.editando.descricao = this.descricao;
+      this.editando.data = this.data;
+      this.editando.ativa = situacao;
+      this.editando.tipo = this.tipo;
+      this.editando.periodo = this.periodo;
       this.editar_ok = true;
     } else {
-      const d = new Disciplina(this.nome, this.descricao);
+      const d = new Disciplina(this.codigo,this.nome, this.descricao, this.data, situacao, this.periodo);
       this.disciplinas.push(d);
+      this.redefinir();
       this.salvar_ok = true;
     }
+    this.codigo = null;
     this.nome = null;
     this.descricao = null;
+    this.data = null;
+    this.ativa = null;
+    this.tipo = 'principal';
+    this.periodo = null;
     this.editando = null;
   }
 
   excluir(disciplina) {
-    this.redefinir();
     if (this.editando == disciplina) {
       alert('Você não pode excluir uma disciplina que está editando');
     } else {
@@ -90,6 +113,7 @@ export class AppComponent {
           + disciplina.nome + '"?')) {
         const i = this.disciplinas.indexOf(disciplina);
         this.disciplinas.splice(i, 1);
+        this.redefinir();
         this.excluir_ok = true;
       }
     }
@@ -97,8 +121,13 @@ export class AppComponent {
 
   editar(disciplina) {
     this.redefinir();
+    this.codigo = disciplina.codigo;
     this.nome = disciplina.nome;
     this.descricao = disciplina.descricao;
+    this.data = disciplina.data;
+    this.ativa = disciplina.ativa;
+    this.tipo = disciplina.tipo;
+    this.periodo = disciplina.periodo;
     this.editando = disciplina;
   }
 
@@ -107,8 +136,14 @@ export class AppComponent {
   }
 
   redefinir() {
+    this.visualizado = null;
+    this.codigo = null;
     this.nome = null;
     this.descricao = null;
+    this.data = null;
+    this.ativa = null;
+    this.tipo = 'principal';
+    this.periodo = null;
     this.editando = null;
     this.excluir_ok = false;
     this.salvar_ok = false;
--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -1,9 +1,21 @@
+import { Attribute } from "@angular/core/src/metadata/di";
+
 export class Disciplina {
+  codigo: number;
   nome: string;
   descricao: string;
+  data: Date;
+  ativa: boolean;
+  tipo: string;
+  periodo: any;
 
-  constructor(nome: string, descricao?: string) {
+  constructor(codigo: number, nome: string, descricao?: string, data?: Date, ativa?: boolean, tipo?: string, periodo?: any) {
+    this.codigo= codigo;
     this.nome = nome;
     this.descricao = descricao;
+    this.data = data;
+    this.ativa = ativa;
+    this.tipo = tipo;
+    this.periodo = periodo;
   }
 }
